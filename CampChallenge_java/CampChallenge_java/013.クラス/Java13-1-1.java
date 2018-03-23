57df58383844b54bdd660efb21c2080340728f40
DESKTOP-7AHFSI8\maka3 <maka343noshi@gmail.com>
2018-03-23 18:03:05 +0900

    Java13-1　クラス　クラスの作成　を提出します。

---
+++ CampChallenge_java/CampChallenge_java/013.クラス/Java13-1-1.java
@@ -0,0 +1,21 @@
+package org.mypackage.sample;
+
+
+public class Java131 {
+    
+    public String name = "";
+    public int money = 0;
+    
+    public void shinamono(String name, int money){
+        this.name = name;
+        this.money = money;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    public int getMoney(){
+        return money;
+    }
+    
+}
