da7621280bc5069c03d7b1db14ad677b352690c1
DESKTOP-7AHFSI8\maka3 <maka343noshi@gmail.com>
2018-03-27 13:47:25 +0900

    Java15-4　標準クラス　タイムスタンプの表示　を提出します。

---
+++ CampChallenge_java/CampChallenge_java/015.標準クラス/Date4.java
@@ -0,0 +1,75 @@
+package method;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Date4 extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            //日時を用意
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 hh時mm分ss秒");
+            Date dateFrom = sdf.parse("2015年1月1日 0時0分0秒");
+            Date dateTo= sdf.parse("2015年12月31日 23時59分59秒");
+            //DateをTimestampに変換
+            long timeFrom = dateFrom.getTime();
+            long timeTo = dateTo.getTime();
+
+            out.print((timeTo - timeFrom) + "秒の差");
+                    
+        } catch (ParseException ex) {
+            Logger.getLogger(Date4.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
