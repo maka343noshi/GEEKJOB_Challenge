a2a4302bfac24f4d4d383b5749a49b42bc7cb953
DESKTOP-7AHFSI8\maka3 <maka343noshi@gmail.com>
2018-03-27 15:40:21 +0900

    Java15-8　標準クラス　ファイルの書き出しと保存　を提出します。

---
+++ CampChallenge_java/CampChallenge_java/015.標準クラス/File1.java
@@ -0,0 +1,71 @@
+package method;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+public class File1 extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            //ファイルクラス生成
+            File file = new File("test.txt");
+            //ファイルを作成
+            if(file.createNewFile()){
+                out.println("ファイルの作成に成功");
+            }else{
+                out.println("ファイルの作成に失敗");
+            }
+            //書き込み
+            FileWriter fw = new FileWriter(file);
+            fw.write("こんにちは。わたしは〇〇です。よろしくお願いします。");
+            fw.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
