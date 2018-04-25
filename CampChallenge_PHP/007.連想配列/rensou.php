b1b7941ea01fd5e62c519900fbd1fc350d6f41b6
DESKTOP-7AHFSI8\maka3 <maka343noshi@gmail.com>
2018-04-25 13:13:14 +0900

    PHP7-1　連想配列の操作　連想配列の作成　を提出します。

---
+++ CampChallenge_PHP/007.連想配列/rensou.php
@@ -0,0 +1,8 @@
+<?php
+
+$data = array('1'=>'AAA', 'hello'=>'world', 'soeda'=>'33', '20'=>'20');
+
+echo $data['1'], '<br>';
+echo $data['hello'], '<br>';
+echo $data['soeda'], '<br>';
+echo $data['20'], '<br>';
