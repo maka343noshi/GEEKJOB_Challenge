94807898182ecf0a7fa1d4732b27d56850624211
DESKTOP-7AHFSI8\maka3 <maka343noshi@gmail.com>
2018-04-25 13:05:53 +0900

    PHP6-1　配列　配列の作成　を提出します。

---
+++ CampChallenge_PHP/006.配列/hairetu01.php
@@ -0,0 +1,14 @@
+<?php
+
+$data = array("10","100","soeda","hayashi","-20","118","END");
+
+echo $data[0],'<br>';
+echo $data[1],'<br>';
+echo $data[2],'<br>';
+echo $data[3],'<br>';
+echo $data[4],'<br>';
+echo $data[5],'<br>';
+echo $data[6],'<br>';
+
+
+
