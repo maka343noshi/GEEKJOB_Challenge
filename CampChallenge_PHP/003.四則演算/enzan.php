faf82a9ca2a9f61aaa2b6dc85f01ab96b65de71b
DESKTOP-7AHFSI8\maka3 <maka343noshi@gmail.com>
2018-04-25 12:01:29 +0900

    PHP3-1　四則演算　変数の宣言と表示（四則演算）　を提出します。

---
+++ CampChallenge_PHP/003.四則演算/enzan.php
@@ -0,0 +1,10 @@
+<?php
+
+const num1 = 124;
+$num2 = 17;
+
+echo '124+17=',(num1 + $num2),'<br>';
+echo '124-17=',(num1 - $num2),'<br>';
+echo '124×17=',(num1 * $num2),'<br>';
+echo '124÷17=',(num1 / $num2),'<br>';
+echo '124%17=',(num1 % $num2),'<br>';
