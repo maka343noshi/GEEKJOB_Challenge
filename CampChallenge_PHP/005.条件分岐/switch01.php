2cb2ed39046be3a6771e728fc187c0ea59678e97
DESKTOP-7AHFSI8\maka3 <maka343noshi@gmail.com>
2018-04-25 12:15:08 +0900

    PHP5-2　switch文　switch文1　を提出します。

---
+++ CampChallenge_PHP/005.条件分岐/switch01.php
@@ -0,0 +1,15 @@
+<?php
+
+$num = 3;
+
+switch($num) {
+    case 1:
+        echo 'one';
+        break;
+    case 2:
+        echo 'two';
+        break;
+    default:
+        echo '想定外';
+        break;
+}
