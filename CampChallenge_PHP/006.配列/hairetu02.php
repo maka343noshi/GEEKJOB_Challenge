ca577d41e88e8ccd2bf83d79e670b68ae899e1a4
DESKTOP-7AHFSI8\maka3 <maka343noshi@gmail.com>
2018-04-25 13:06:21 +0900

    PHP6-2　配列　配列の変更　を提出します。

---
+++ CampChallenge_PHP/006.配列/hairetu02.php
@@ -0,0 +1,13 @@
+<?php
+
+$data = array("10","100","soeda","hayashi","-20","118","END");
+
+$data[2] = "33";
+
+echo $data[0],'<br>';
+echo $data[1],'<br>';
+echo $data[2],'<br>';
+echo $data[3],'<br>';
+echo $data[4],'<br>';
+echo $data[5],'<br>';
+echo $data[6],'<br>';
