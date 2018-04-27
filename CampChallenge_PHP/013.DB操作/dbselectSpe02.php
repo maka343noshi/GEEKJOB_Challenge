7651025580e68c24095081a9b3bb2a376af89fc0
DESKTOP-7AHFSI8\maka3 <maka343noshi@gmail.com>
2018-04-27 18:17:31 +0900

    PHP13-5　DB操作　特定のレコードの情報取得と表示　を提出します。

---
+++ CampChallenge_PHP/013.DB操作/dbselectSpe02.php
@@ -0,0 +1,39 @@
+<?php
+
+try{
+    $dns =  'mysql:host=localhost;';
+    $dns .= 'dbname=Challenge_db;';
+    $dns .= 'charset=utf8';
+    // データベースへ接続
+    $pdo = new PDO($dns, 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $name = '松';    
+    //sql文の作成
+    $sql = 'select * from profiles where name like :name';
+    $stmt = $pdo->prepare($sql);
+
+    //:nameの置き換え
+    $stmt->bindValue(':name', '%'.$name.'%');
+    $stmt->execute();
+
+    // 実行結果を連想配列で取得
+    $datas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    //表示
+    foreach($datas as $recod){
+        foreach ($recod as $key => $value) {
+            echo $key. ' : '. $value. '<br>';
+        }
+        echo '<br>';
+    }
+
+    
+    $pdo = null;
+
+    echo '成功';
+} catch (PDOException $e) {
+    echo 'DB接続に失敗しました。' . $e->getMessage();
+}
+
+
