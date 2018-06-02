376c5eac9b2f286f5de2d3922ddc7f51d019cd98
DESKTOP-7AHFSI8\maka3 <maka343noshi@gmail.com>
2018-06-02 16:22:37 +0900

    PHP17-1　巨大なソースの修正　第一段階　を提出します

--- CampChallenge_PHP/017.巨大なソースの修正/UserManagementSystem-ver1.0/app/insert.php
+++ CampChallenge_PHP/017.巨大なソースの修正/UserManagementSystem-ver1.0/app/insert.php
@@ -1,4 +1,5 @@
 <?php require_once '../common/defineUtil.php'; ?>
+<?php require_once '../common/scriptUtil.php'; ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -6,51 +7,57 @@
       <title>登録画面</title>
 </head>
 <body>
+    <?php session_start();//再入力時用 ?>
     <form action="<?php echo INSERT_CONFIRM ?>" method="POST">
-    名前:
-    <input type="text" name="name">
-    <br><br>
 
-    生年月日:
-    <select name="year">
-        <option value="----">----</option>
+        名前:
+        <input type="text" name="name" value="<?php echo form_value('name'); ?>">
+        <br><br>
+
+        生年月日:
+        <select name="year">
+            <option value="">----</option>
+            <?php
+            for($i=1950; $i<=2010; $i++){ ?>
+            <option value="<?php echo $i;?>" <?php if($i==form_value('year')){echo "selected";}?>><?php echo $i ;?></option>
+            <?php } ?>
+        </select>年
+        <select name="month">
+            <option value="">--</option>
+            <?php
+            for($i = 1; $i<=12; $i++){?>
+            <option value="<?php echo $i;?>" <?php if($i==form_value('month')){echo "selected";}?>><?php echo $i;?></option>
+            <?php } ;?>
+        </select>月
+        <select name="day">
+            <option value="">--</option>
+            <?php
+            for($i = 1; $i<=31; $i++){ ?>
+            <option value="<?php echo $i; ?>"<?php if($i==form_value('day')){echo "selected";}?>><?php echo $i;?></option>
+            <?php } ?>
+        </select>日
+        <br><br>
+
+        種別:
+        <br>
         <?php
-        for($i=1950; $i<=2010; $i++){ ?>
-        <option value="<?php echo $i;?>"><?php echo $i ;?></option>
+        for($i = 1; $i<=3; $i++){ ?>
+        <input type="radio" name="type" value="<?php echo $i; ?>"<?php if($i==form_value('type')){echo "checked";}?>><?php echo ex_typenum($i);?><br>
         <?php } ?>
-    </select>年
-    <select name="month">
-        <option value="--">--</option>
-        <?php
-        for($i = 1; $i<=12; $i++){?>
-        <option value="<?php echo $i;?>"><?php echo $i;?></option>
-        <?php } ;?>
-    </select>月
-    <select name="day">
-        <option value="--">--</option>
-        <?php
-        for($i = 1; $i<=31; $i++){ ?>
-        <option value="<?php echo $i; ?>"><?php echo $i;?></option>
-        <?php } ?>
-    </select>日
-    <br><br>
+        <br>
 
-    種別:
-    <br>
-    <input type="radio" name="type" value="エンジニア" checked>エンジニア<br>
-    <input type="radio" name="type" value="営業">営業<br>
-    <input type="radio" name="type" value="その他">その他<br>
-    <br>
+        電話番号:
+        <input type="text" name="tell" value="<?php echo form_value('tell'); ?>">
+        <br><br>
 
-    電話番号:
-    <input type="text" name="tell">
-    <br><br>
+        自己紹介文
+        <br>
+        <textarea name="comment" rows=10 cols=50 style="resize:none" wrap="hard"><?php echo form_value('comment'); ?></textarea><br><br>
 
-    自己紹介文
-    <br>
-    <textarea name="comment" rows=10 cols=50 style="resize:none" wrap="hard"></textarea><br><br>
-
-    <input type="submit" name="btnSubmit" value="確認画面へ">
+        <input type="hidden" name="mode"  value="CONFIRM">
+        <input type="submit" name="btnSubmit" value="確認画面へ">
     </form>
+
+    <?php echo return_top(); ?>
 </body>
 </html>
--- CampChallenge_PHP/017.巨大なソースの修正/UserManagementSystem-ver1.0/app/insert_result.php
+++ CampChallenge_PHP/017.巨大なソースの修正/UserManagementSystem-ver1.0/app/insert_result.php
@@ -8,51 +8,39 @@
 <meta charset="UTF-8">
       <title>登録結果画面</title>
 </head>
-<body>
-
+    <body>
     <?php
-    session_start();
-    $name = $_SESSION['name'];
-    $birthday = $_SESSION['birthday'];
-    $type = $_SESSION['type'];
-    $tell = $_SESSION['tell'];
-    $comment = $_SESSION['comment'];
+    if(!$_POST['mode']=="RESULT"){
+        echo 'アクセスルートが不正です。もう一度トップページからやり直してください<br>';
+    }else{
+        
+        session_start();
+        $name = $_SESSION['name'];
+        //date型にするために1桁の月日を2桁にフォーマットしてから格納
+        $birthday = $_SESSION['year'].'-'.sprintf('%02d',$_SESSION['month']).'-'.sprintf('%02d',$_SESSION['day']);
+        $type = $_SESSION['type'];
+        $tell = $_SESSION['tell'];
+        $comment = $_SESSION['comment'];
 
-    //db接続を確立
-    $insert_db = connect2MySQL();
-    
-    //DBに全項目のある1レコードを登録するSQL
-    $insert_sql = "INSERT INTO user_t(name,birthday,tell,type,comment,newDate)"
-            . "VALUES(:name,:birthday,:tell,:type,:comment,:newDate)";
-
-    //クエリとして用意
-    $insert_query = $insert_db->prepare($insert_sql);
-
-    //SQL文にセッションから受け取った値＆現在時をバインド
-    $insert_query->bindValue(':name',$name);
-    $insert_query->bindValue(':birthday',$birthday);
-    $insert_query->bindValue(':tell',$tell);
-    $insert_query->bindValue(':type',$type);
-    $insert_query->bindValue(':comment',$comment);
-    $insert_query->bindValue(':newDate',time());
-    
-    //SQLを実行
-    $insert_query->execute();
-    
-    //接続オブジェクトを初期化することでDB接続を切断
-    $insert_db=null;
+        //データのDB挿入処理。エラーの場合のみエラー文がセットされる。成功すればnull
+        $result = insert_profiles($name, $birthday, $type, $tell, $comment);
+        
+        //エラーが発生しなければ表示を行う
+        if(!isset($result)){
+        ?>
+        <h1>登録結果画面</h1><br>
+        名前:<?php echo $name;?><br>
+        生年月日:<?php echo $birthday;?><br>
+        種別:<?php echo ex_typenum($type);?><br>
+        電話番号:<?php echo $tell;?><br>
+        自己紹介:<?php echo $comment;?><br><br>
+        以上の内容で登録しました。<br>
+        <?php
+        }else{
+            echo 'データの挿入に失敗しました。次記のエラーにより処理を中断します:'.$result;
+        }
+    }
+    echo return_top(); 
     ?>
-
-    <h1>登録結果画面</h1><br>
-    名前:<?php echo $name;?><br>
-    生年月日:<?php echo $birthday;?><br>
-    種別:<?php echo $type?><br>
-    電話番号:<?php echo $tell;?><br>
-    自己紹介:<?php echo $comment;?><br>
-    以上の内容で登録しました。<br>
-
-    <?php echo return_top(); ?>
-    
-</body>
-
+    </body>
 </html>
